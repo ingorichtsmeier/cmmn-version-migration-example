1edfd0fff4e8d02ebe4fddde5c08ecdcb6e7e5ae
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2023-01-12 15:13:36 +0100

    full qualified insert statement

--- src/test/java/com/camunda/consulting/CmmnVersionMigrationTest.java
+++ src/test/java/com/camunda/consulting/CmmnVersionMigrationTest.java
@@ -90,12 +90,20 @@ public class CmmnVersionMigrationTest {
     updateCaseDefInCaseExecution(caseInstance.getId(), caseInstance2.getCaseDefinitionId());
     displayCaseExecutions();
 
-    // insert into act_ru_case_execution values ('100', 1, '4', null, null, null, '4', 'Case_1:2:9', 'PlanItem_2', 0, 1, false, null);
-    updateDatabaseContent(String.format("insert into act_ru_case_execution values ('100', 1, '%s', null, null, null, '%s', '%s', 'PlanItem_2', 0, 1, false, null)",
+    // insert into act_ru_case_execution 
+    // (ID_, REV_, CASE_INST_ID_, SUPER_CASE_EXEC_, SUPER_EXEC_, BUSINESS_KEY_, PARENT_ID_, CASE_DEF_ID_, ACT_ID_, PREV_STATE_, CURRENT_STATE_, REQUIRED_, TENANT_ID_) 
+    // values ('100', 1, '4', null, null, null, '4', 'Case_1:2:9', 'PlanItem_2', 0, 1, false, null);
+    updateDatabaseContent(String.format("insert into act_ru_case_execution" 
+        + " (ID_, REV_, CASE_INST_ID_, SUPER_CASE_EXEC_, SUPER_EXEC_, BUSINESS_KEY_, PARENT_ID_, CASE_DEF_ID_, ACT_ID_, PREV_STATE_, CURRENT_STATE_, REQUIRED_, TENANT_ID_)"
+        + " values ('100', 1, '%s', null, null, null, '%s', '%s', 'PlanItem_2', 0, 1, false, null)",
         caseInstance.getId(), caseInstance.getId(), caseInstance2.getCaseDefinitionId()));
 
-    // insert into act_ru_case_sentry_part values ('101',1,'4','4','Sentry_0m595h6','ifPart',null,null,null,null,null,false,null);
-    updateDatabaseContent(String.format("insert into act_ru_case_sentry_part values ('101',1,'%s','%s','Sentry_0m595h6','ifPart',null,null,null,null,null,false,null)",
+    // insert into act_ru_case_sentry_part 
+    // (ID_, REV_, CASE_INST_ID_, CASE_EXEC_ID_, SENTRY_ID_, TYPE_, SOURCE_CASE_EXEC_ID_, STANDARD_EVENT_, SOURCE_, VARIABLE_EVENT_, VARIABLE_NAME_, SATISFIED_, TENANT_ID_) 
+    // values ('101',1,'4','4','Sentry_0m595h6','ifPart',null,null,null,null,null,false,null);
+    updateDatabaseContent(String.format("insert into act_ru_case_sentry_part"
+        + " (ID_, REV_, CASE_INST_ID_, CASE_EXEC_ID_, SENTRY_ID_, TYPE_, SOURCE_CASE_EXEC_ID_, STANDARD_EVENT_, SOURCE_, VARIABLE_EVENT_, VARIABLE_NAME_, SATISFIED_, TENANT_ID_)"
+        + " values ('101',1,'%s','%s','Sentry_0m595h6','ifPart',null,null,null,null,null,false,null)",
         caseInstance.getId(), caseInstance.getId()));
 
     caseInstance = caseService().createCaseInstanceQuery().caseInstanceId(caseInstance.getId()).singleResult();
